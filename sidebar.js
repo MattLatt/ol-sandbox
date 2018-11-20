9c97aaf245c728166f6b8892c271dc66c6fb822e
userlx <userlx@pchome>
2018-11-20 20:24:45 +0100

    stash

--- sidebar.js
+++ sidebar.js
@@ -4,8 +4,8 @@
         projection: 'EPSG:3857',
         // comment the following two lines to have the mouse position
         // be placed within the map.
-        //className: 'custom-mouse-position',
-        //target: document.getElementById('mouse-position'),
+        className: 'custom-mouse-position',
+        target: document.getElementById('mouse-position'),
         undefinedHTML: '&nbsp;'
       });
   
